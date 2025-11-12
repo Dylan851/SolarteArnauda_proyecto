c13a4349fc9d3f25701cf0946b1bd089d3eba346
Dylan851 <0267dsolarte@e-itaca.es>
2025-11-12 20:08:18 +0100

    buttomnavegationbar

---
+++ lib/config/resources/appColor.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class Appcolor {
+  static const Color backgroundColor = const Color.fromARGB(255, 230, 14, 14);
+  static const Color accent = Color(0xFFEB1555);
+  static const Color scaffoldBackground = Color(0xFFFFFFFF);
+}
